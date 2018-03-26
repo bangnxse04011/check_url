0968377dff4c3502937d23134a2d825ceefa55a8
bangnx1 <bangnx1@fsoft.com.vn>
2018-03-27 00:14:09 +0700

    update code

--- check_url/public/javascripts/unit.js
+++ check_url/public/javascripts/unit.js
@@ -7,5 +7,23 @@ module.exports = {
         let api_key = "36129368de4ed64827fc3440f53d5e6d972cdc0aa8c0832d292c6b96ffd86974";
         let api_with_url = "https://www.virustotal.com/vtapi/v2/url/report?apikey=" + api_key + "&resource=" + url;
         return api_with_url;
+    },
+    /**
+     * Method detect keyword
+     */
+    common_keyword_detect_url: () => {
+        let list_keyword = ['sex', 'game', 'jav', 'xxx', 'thiendia', 'khieudam', '18+'];
+        return list_keyword;
+    },
+    /**
+     * Check URL valid
+     */
+    common_valid_URL: (str) => {
+        var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+        if (!regex.test(str)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- check_url/routes/index.js
+++ check_url/routes/index.js
@@ -8,7 +8,7 @@ var unit = require('../public/javascripts/unit');
 * GET home page. 
 */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Kiểm tra URL', result_check_url: '', _url_scan: '' });
+  res.render('index', { title: 'Kiểm tra URL', result_check_url: '', _url_scan: '', mess: '' });
 });
 
 /**
@@ -17,6 +17,23 @@ router.get('/', function (req, res, next) {
 router.get('/check_url', (req, resp) => {
   // Get URL from form.
   let _url_check = req.query.url_scan;
+  if (_url_check.trim() == '' || _url_check.trim() == "") {
+    resp.redirect("/");
+  }
+  if (!unit.common_valid_URL(_url_check)) {
+    resp.render('index', { title: 'Kiểm tra URL', result_check_url: '', _url_scan: '', mess: 'Bạn có biết URL nó thế nào ko?' });
+  }
+  // Check in list
+  let list_keyword = unit.common_keyword_detect_url();
+  for (let index = 0; index < list_keyword.length; index++) {
+    if (_url_check.trim().indexOf(list_keyword[index]) > 0) {
+      let result_check_url = {};
+      result_check_url.positives = -1;
+      result_check_url.permalink = '';
+      result_check_url.total = '-';
+      resp.render('index', { title: 'Kiểm tra URL', result_check_url: result_check_url, _url_scan: _url_check, mess: '' })
+    }
+  }
   // Create key and URL check
   let _url = unit.common_setting_api(_url_check);
 
@@ -26,7 +43,7 @@ router.get('/check_url', (req, resp) => {
   }
   axios(params)
     .then(res =>
-      resp.render('index', { title: 'Kiểm tra URL', result_check_url: res.data, _url_scan: _url_check })
+      resp.render('index', { title: 'Kiểm tra URL', result_check_url: res.data, _url_scan: _url_check, mess: '' })
     ).catch(error => {
       console.error(error)
     })
